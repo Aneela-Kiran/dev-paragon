bfc2f73a001662409d6a299facdc615a8cb478d8
Aneela kiran <aneelakiran839@gmail.com>
2025-08-03 13:56:18 -0700

    update

--- src/components/Deliverable.jsx
+++ src/components/Deliverable.jsx
@@ -4,7 +4,7 @@ import ProjectCard from "./cards";
 import AchievementCard from "./AchievementCard";
 import bgImage from "../assets/bg.jpg";
 
-function Deliverable() {
+function Deliverable() {  
   return (
     <section className="my-40 overflow-hidden lg:px-24 ">
       <div className="bg-[#0b0f1b] text-white py-12 md:px-12 relative overflow-hidden max-w-[1440px] mx-auto">
@@ -16,7 +16,6 @@ function Deliverable() {
             className="absolute top-0 left-0 w-[200px] h-[500px] opacity-25 pointer-events-none select-none z-0 "
           />
         </div> */}
-
         {/* Top Heading */}
         <div className="flex flex-col lg:flex-row justify-between lg:items-center items-start my-6 px-10 py-8 lg:py-4">
           <div className="mt-5">
@@ -32,15 +31,16 @@ function Deliverable() {
           </div>
         </div>
         {/* Cards Section */}
-        <div>
-          <ProjectCard />
+          <div>
+            <ProjectCard />
+          </div>
+          {/* Achievement Section */}
+          <div>
+            <AchievementCard />
+          </div>
         </div>
-        {/* Achievement Section */}
-        <div>
-          <AchievementCard />
-        </div>
-      </div>
     </section>
+
   );
 }
 
